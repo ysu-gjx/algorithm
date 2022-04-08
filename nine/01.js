bd8890929555def658969c4f19d89e15fa91c54e
ysu-gjx <252932110@qq.com>
2022-04-08 19:32:57 +0800

    init

---
+++ nine/01.js
@@ -0,0 +1,33 @@
+/**
+ * 最长回文子串
+ * longest-palindromic-substring
+   动态规划和枚举法都必须掌握
+ *
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+  var longestPalindrome = function(s) {
+    
+  }
+
+/**
+ * 实现strstr
+ * implement-strstr
+ */
+
+/**
+ * Robin-Karp 方法
+ */
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+  var strStr = function(haystack, needle) {
+
+  }
+
